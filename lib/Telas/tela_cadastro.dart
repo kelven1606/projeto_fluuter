9e2725eabc8a213e398bc4cb3abf21e73bf5d465
GISELY_DA_SILVA_BRITO <gsb5@aluno.ifal.edu.br>
2023-08-14 19:06:17 +0000

    gg

--- lib/Telas/tela_cadastro.dart
+++ lib/Telas/tela_cadastro.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:libras2/domain/Cadastros.dart';
+import 'package:libras2/db/CadastroDao.dart';
 
-class Cadastro extends StatelessWidget {
-  const Cadastro({super.key});
+import 'TelaPrincipal.dart';
+
+class Cadastro extends StatefulWidget {
+  Cadastro({super.key});
+
+  @override
+  State<Cadastro> createState() => _CadastroState();
+}
+
+class _CadastroState extends State<Cadastro> {
+  bool confirmeSenha = true;
+  bool confirmeSenha1 = true;
+
+  Future<List<Cadastros>> futureLista = CadastroDao().findAll();
 
   @override
   Widget build(BuildContext context) {
@@ -17,147 +31,195 @@ class Cadastro extends StatelessWidget {
         ),
         backgroundColor: Colors.white,
         body: Container(
-          padding: EdgeInsets.all(60),
-          child: ListView(
-            children: <Widget>[
-              TextFormField(
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          style: BorderStyle.solid, color: Colors.purple),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          style: BorderStyle.solid, color: Colors.purple),
-                    ),
-                    labelText: "Nome",
-                    prefixIcon: Padding(
-                      child: Icon(Icons.person, color: Colors.purple),
-                      padding: EdgeInsets.all(5),
-                    ),
-                    labelStyle: TextStyle(
-                      color: Colors.black38,
-                      fontWeight: FontWeight.w400,
-                      fontSize: 20,
-                    )),
-                style: TextStyle(fontSize: 20),
+            padding: EdgeInsets.all(60),
+            child: FutureBuilder<List<Cadastros>>(
+              future: futureLista,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  List<Cadastros> lista = snapshot.data!;
+                  return buildListView(lista[0]);
+                }
+
+                return Center(child: CircularProgressIndicator());
+              },
+            )),
+      ),
+    );
+  }
+
+  buildListView(Cadastros cadastro) {
+    return ListView(
+      children: <Widget>[
+        TextFormField(
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(
+              enabledBorder: UnderlineInputBorder(
+                borderSide:
+                    BorderSide(style: BorderStyle.solid, color: Colors.purple),
               ),
-              SizedBox(height: 10),
-              TextFormField(
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          style: BorderStyle.solid, color: Colors.purple),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          style: BorderStyle.solid, color: Colors.purple),
-                    ),
-                    labelText: "Sobrenome",
-                    prefixIcon: Padding(
-                      child: Icon(Icons.person, color: Colors.purple),
-                      padding: EdgeInsets.all(5),
-                    ),
-                    labelStyle: TextStyle(
-                      color: Colors.black38,
-                      fontWeight: FontWeight.w400,
-                      fontSize: 20,
-                    )),
-                style: TextStyle(fontSize: 20),
+              focusedBorder: UnderlineInputBorder(
+                borderSide:
+                    BorderSide(style: BorderStyle.solid, color: Colors.purple),
               ),
-              SizedBox(height: 10),
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          style: BorderStyle.solid, color: Colors.purple),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          style: BorderStyle.solid, color: Colors.purple),
-                    ),
-                    labelText: "Digite seu email",
-                    prefixIcon: Padding(
-                      child: Icon(Icons.email, color: Colors.purple),
-                      padding: EdgeInsets.all(5),
-                    ),
-                    labelStyle: TextStyle(
-                      color: Colors.black38,
-                      fontWeight: FontWeight.w400,
-                      fontSize: 20,
-                    )),
-                style: TextStyle(fontSize: 20),
+              labelText: cadastro.nome,
+              prefixIcon: Padding(
+                child: Icon(Icons.person, color: Colors.purple),
+                padding: EdgeInsets.all(5),
               ),
-              SizedBox(height: 10),
-              TextFormField(
-                keyboardType: TextInputType.text,
-                obscureText: true,
-                decoration: InputDecoration(
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          style: BorderStyle.solid, color: Colors.purple),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          style: BorderStyle.solid, color: Colors.purple),
-                    ),
-                    labelText: "Crie uma senha",
-                    prefixIcon: Padding(
-                      child: Icon(Icons.key, color: Colors.purple),
-                      padding: EdgeInsets.all(5),
-                    ),
-                    labelStyle: TextStyle(
-                      color: Colors.black38,
-                      fontWeight: FontWeight.w400,
-                      fontSize: 20,
-                    )),
-                style: TextStyle(fontSize: 20),
+              labelStyle: TextStyle(
+                color: Colors.black38,
+                fontWeight: FontWeight.w400,
+                fontSize: 20,
+              )),
+          style: TextStyle(fontSize: 20),
+        ),
+        SizedBox(height: 10),
+        TextFormField(
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(
+              enabledBorder: UnderlineInputBorder(
+                borderSide:
+                    BorderSide(style: BorderStyle.solid, color: Colors.purple),
               ),
-              SizedBox(height: 10),
-              TextFormField(
-                keyboardType: TextInputType.text,
-                obscureText: true,
-                decoration: InputDecoration(
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          style: BorderStyle.solid, color: Colors.purple),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          style: BorderStyle.solid, color: Colors.purple),
-                    ),
-                    labelText: "Confirme sua senha",
-                    prefixIcon: Padding(
-                      child: Icon(Icons.key, color: Colors.purple),
-                      padding: EdgeInsets.all(5),
-                    ),
-                    labelStyle: TextStyle(
-                      color: Colors.black38,
-                      fontWeight: FontWeight.w400,
-                      fontSize: 20,
-                    )),
-                style: TextStyle(fontSize: 20),
+              focusedBorder: UnderlineInputBorder(
+                borderSide:
+                    BorderSide(style: BorderStyle.solid, color: Colors.purple),
               ),
-              SizedBox(height: 60),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    backgroundColor: Colors.purple),
-                //style: ElevatedButton.styleFrom(backgroundColor: Colors.purple),//
-                onPressed: () {},
-                child: Text(
-                  'Cadastrar',
-                  style: TextStyle(color: Colors.white, fontSize: 20),
+              labelText: cadastro.sobrenome,
+              prefixIcon: Padding(
+                child: Icon(Icons.person, color: Colors.purple),
+                padding: EdgeInsets.all(5),
+              ),
+              labelStyle: TextStyle(
+                color: Colors.black38,
+                fontWeight: FontWeight.w400,
+                fontSize: 20,
+              )),
+          style: TextStyle(fontSize: 20),
+        ),
+        SizedBox(height: 10),
+        TextFormField(
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+              enabledBorder: UnderlineInputBorder(
+                borderSide:
+                    BorderSide(style: BorderStyle.solid, color: Colors.purple),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide:
+                    BorderSide(style: BorderStyle.solid, color: Colors.purple),
+              ),
+              labelText: cadastro.email,
+              prefixIcon: Padding(
+                child: Icon(Icons.email, color: Colors.purple),
+                padding: EdgeInsets.all(5),
+              ),
+              labelStyle: TextStyle(
+                color: Colors.black38,
+                fontWeight: FontWeight.w400,
+                fontSize: 20,
+              )),
+          style: TextStyle(fontSize: 20),
+        ),
+        SizedBox(height: 10),
+        TextFormField(
+          keyboardType: TextInputType.text,
+          obscureText: confirmeSenha1,
+          decoration: InputDecoration(
+              suffixIcon: InkWell(
+                // serve para deixar o icone cliclável
+                child: Icon(
+                  confirmeSenha1
+                      ? Icons.remove_red_eye_outlined
+                      : Icons.remove_red_eye,
+                  color: Colors.purple,
                 ),
+                onTap: () {
+                  // ! => Mudar de true ´/ false ou de false p/ true
+                  setState(() {
+                    //serve para avisar o flutter que a tela atualizou, não funciona com a tela statele, só statful
+                    confirmeSenha1 = !confirmeSenha1;
+                  });
+                },
               ),
-            ],
+              enabledBorder: const UnderlineInputBorder(
+                borderSide:
+                    BorderSide(style: BorderStyle.solid, color: Colors.purple),
+              ),
+              focusedBorder: const UnderlineInputBorder(
+                borderSide:
+                    BorderSide(style: BorderStyle.solid, color: Colors.purple),
+              ),
+              labelText: cadastro.senha1,
+              prefixIcon: const Padding(
+                child: const Icon(Icons.key, color: Colors.purple),
+                padding: EdgeInsets.all(5),
+              ),
+              labelStyle: const TextStyle(
+                color: Colors.black38,
+                fontWeight: FontWeight.w400,
+                fontSize: 20,
+              )),
+          style: TextStyle(fontSize: 20),
+        ),
+        SizedBox(height: 10),
+        TextFormField(
+          keyboardType: TextInputType.text,
+          obscureText: confirmeSenha,
+          decoration: InputDecoration(
+              suffixIcon: InkWell(
+                // serve para deixar o icone cliclável
+                child: Icon(
+                  confirmeSenha
+                      ? Icons.remove_red_eye_outlined
+                      : Icons.remove_red_eye,
+                  color: Colors.purple,
+                ),
+                onTap: () {
+                  // ! => Mudar de true ´/ false ou de false p/ true
+                  setState(() {
+                    //serve para avisar o flutter que a tela atualizou, não funciona com a tela statele, só statful
+                    confirmeSenha = !confirmeSenha;
+                  });
+                },
+              ),
+              enabledBorder: const UnderlineInputBorder(
+                borderSide:
+                    BorderSide(style: BorderStyle.solid, color: Colors.purple),
+              ),
+              focusedBorder: const UnderlineInputBorder(
+                borderSide:
+                    BorderSide(style: BorderStyle.solid, color: Colors.purple),
+              ),
+              labelText: cadastro.senha2,
+              prefixIcon: const Padding(
+                child: const Icon(Icons.key, color: Colors.purple),
+                padding: EdgeInsets.all(5),
+              ),
+              labelStyle: const TextStyle(
+                color: Colors.black38,
+                fontWeight: FontWeight.w400,
+                fontSize: 20,
+              )),
+          style: TextStyle(fontSize: 20),
+        ),
+        SizedBox(height: 60),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(backgroundColor: Colors.purple),
+          //style: ElevatedButton.styleFrom(backgroundColor: Colors.purple),//
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => TelaPrincipal(),
+              ),
+            );
+          },
+          child: Text(
+            'Cadastrar',
+            style: TextStyle(color: Colors.white, fontSize: 20),
           ),
         ),
-      ),
+      ],
     );
   }
 }
